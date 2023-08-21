2ad09da71d18940480f19d401a9742a0b2f4da56
noumanmoeen <moeennouman7@gmail.com>
2023-08-21 16:31:49 +0500

    Header completed for details page

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -5,40 +5,33 @@ function Details() {
   return (
     <div className='p-6'>
       <div className='flex justify-around '>
-        <div  className='h-30 w-full py-5 px-7  border rounded-md shadow-lg bg-secondary mb-10'>
+        <div className='h-30 w-full py-5 px-7  border rounded-md shadow-lg bg-secondary mb-10'>
           <div className='flex justify-between  '>
             <div className='flex flex-col justify-end'>
-            <p className='text-4xl' >
-              Budget Details
-            </p>
+              <p className='text-4xl'>Budget Details</p>
             </div>
-           
-            <div>
-              <p>
-                Active Budget
-              </p>
-              <p>
-                Budget 1
-              </p>
-              <p>
-                Active Budget
-              </p>
-              <p>
-                Budget 1
-              </p>
-              <p>
-                Active Budget
-              </p>
-              <p>
-                Budget 1
-              </p>
+
+            {/* <div className=''> */}
+            <div className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 gap-3'>
+              <p>Budget Name</p>
+              <p>Budget 1</p>
+              <p>Budget ID</p>
+              <p>123</p>
+            </div>
+            <div className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 gap-3'>
+              <p>Account Number</p>
+              <p>1234</p>
+              <p>User Bank</p>
+              <p>Meezan Bank</p>
+            </div>
+            <div className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 gap-3'>
+              {' '}
+              <p>Saving Goals</p>
+              <p>Rs 2000</p>
             </div>
           </div>
         </div>
-        {/* <div  className=' h-52 w-1/4 p-4 border border-text'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-        
-        </div> */}
+     
       </div>
       <DetailsTabs />
     </div>
