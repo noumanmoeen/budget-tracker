8c6b88803c601c126c7df87135231ffe0c1de934
noumanmoeen <moeennouman7@gmail.com>
2023-09-07 17:13:50 +0500

    Minor issue fixes for budget add moal

--- src/components/BudgetAdd.js
+++ src/components/BudgetAdd.js
@@ -24,7 +24,7 @@ const BudgetAdd = ({ showModal, setShowModal }) => {
     <>
       {showModal ? (
         <>
-          <div className='justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none '>
+          <div className='bg-modalBg justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none '>
             <div className='relative w-full my-6 mx-auto '>
               <div className='border-0 rounded-lg shadow-lg relative flex flex-col w-1/3 mx-auto bg-secondary outline-none focus:outline-none'>
                 <div className='flex items-start justify-between p-5 rounded-t'>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ module.exports = {
         secondary: '#6D9886',
         disabled: '#a4c6e8',
         cancel: '#891337',
+        modalBg: '#00000080',
       },
       fontFamily: {
         sans: 'Preahvihear , sans-serif',
