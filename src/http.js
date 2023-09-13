731b20146eea4a0e9c8cf8425bbea8863df7d131
noumanmoeen <moeennouman7@gmail.com>
2023-09-13 16:23:45 +0500

    - Added axios configurations and added publicroute

--- src/http.js
+++ src/http.js
@@ -1,33 +1,34 @@
 import axios from 'axios';
 
-
 export const axiosInstance = axios.create({
   baseURL: 'http://localhost:5000',
-  timeout: 1000,
 });
-
-
 
 axiosInstance.interceptors.request.use(
   function (req) {
-    console.log('HERE 3',req)
-    req.headers['Authorization'] = 'Bearer tokeneyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1MDA1OTMyMWYyNDA4Y2I0M2YzZGU1NiIsImlhdCI6MTY5NDUyMTY1MSwiZXhwIjoxNjk3MTEzNjUxfQ.GO7YwUahB31p4pUV0xo8rQ0PnF2gYFvukBbPqhpGT9M'
+    const token = localStorage.getItem('token')
+    console.log("HERE 17", token)
+    req.headers['Authorization'] =
+      `Bearer ${token}`;
 
-    return req
+    return req;
   },
-  function(err) {
-    console.log('HERE 4',err)
-    // return Promise.reject(err);
+  function (err) {
+    return Promise.reject(err);
   }
 );
-
-axiosInstance.interceptors.response.use(function(res) {
-  console.log('HERE 2',res)
-  return res;
-},
-function (err) {
-  console.log('HERE 1',err)
-  // return Promise.reject(err);
-});
-
-
+ 
+// axiosInstance.interceptors.response.use(
+//   async (res) => {
+//     let parsedRes;
+//     if (Boolean(res)) {
+//       parsedRes = await res.json();
+//       const { data = {} } = parsedRes;
+//       return data;
+//     }
+//     return res;
+//   },
+//   function (err) {
+//     return Promise.reject(err);
+//   }
+// );
